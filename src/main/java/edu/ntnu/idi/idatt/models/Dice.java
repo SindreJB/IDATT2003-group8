00a67e22313d1d4a4ae13c4c4529bf7f4d5034a6
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-05 13:14:36 +0100

    feat(Dice): Made list for several dice and rolling dice function

---
+++ src/main/java/edu/ntnu/idi/idatt/models/Dice.java
@@ -0,0 +1,20 @@
+package edu.ntnu.idi.idatt.models;
+
+import java.util.List;
+
+public class Dice {
+
+  private List<Die> dice; // List for the option of adding more dice
+
+  public Dice() {
+    this.dice = List.of(new Die(), new Die()); // Two dice
+  }
+
+  public int rollDice() {
+    return dice.stream().mapToInt(Die::roll).sum();
+  }
+
+  public List<Die> getDice() {
+    return dice;
+  }
+}
