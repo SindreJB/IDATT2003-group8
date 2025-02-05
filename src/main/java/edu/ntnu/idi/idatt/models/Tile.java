1c98b9efed0d27d83dfc6b03a72e32d9eaff409c
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-05 13:15:19 +0100

    feat(Tile): Framework for tile

--- src/main/java/edu/ntnu/idi/idatt/models/Tile.java
+++ src/main/java/edu/ntnu/idi/idatt/models/Tile.java
@@ -1,4 +1,18 @@
 package edu.ntnu.idi.idatt.models;
 
 public class Tile {
+  private Tile nextTile;
+  private int tileId;
+  private TileAction landAction;
+
+  public Tile(int tileId) {
+    this.tileId = tileId;
+  }
+
+  public int getTileId() {
+    return tileId;
+  }
+  public void landPlayer(Player player) {}
+  public void leavePlayer(Player player) {}
+  public void setNextTile(Tile nextTile) {}
 }
