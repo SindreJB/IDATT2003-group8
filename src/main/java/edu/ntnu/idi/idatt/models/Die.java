620926e21ec69a7b0c2c93a47a4a7f6c3a0b1d44
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-05 13:13:52 +0100

    feat(Die): Made logic for single dice

--- src/main/java/edu/ntnu/idi/idatt/models/Die.java
+++ src/main/java/edu/ntnu/idi/idatt/models/Die.java
@@ -1,4 +1,22 @@
 package edu.ntnu.idi.idatt.models;
 
+import java.util.Random;
+
 public class Die {
+
+  private int lastRolledValue;
+  private Random random;
+
+  public Die() {
+    this.random = new Random();
+  }
+
+  public int roll() {
+    lastRolledValue = random.nextInt(6) + 1;
+    return lastRolledValue;
+  }
+
+  public int getLastRolledValue() {
+    return lastRolledValue;
+  }
 }
