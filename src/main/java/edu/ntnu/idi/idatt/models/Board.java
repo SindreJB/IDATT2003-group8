fb5cfc79bf0014f4f191b5218823d9e381506f51
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-05 13:18:23 +0100

    feat(Board): Made board of 90 tiles as well as link function ensuring smooth play

--- src/main/java/edu/ntnu/idi/idatt/models/Board.java
+++ src/main/java/edu/ntnu/idi/idatt/models/Board.java
@@ -1,4 +1,29 @@
 package edu.ntnu.idi.idatt.models;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Board {
+  private Map<Integer, Tile> tiles = new HashMap<>();
+
+  public Board() {
+    initializeBoard();
+  }
+
+  public void initializeBoard() {
+    tiles = java.util.stream.IntStream.rangeClosed(1, 90)
+        .boxed()
+        .collect(java.util.stream.Collectors.toMap(i -> i, Tile::new));
+    linkTiles();
+  }
+
+  private void linkTiles() {
+    tiles.values().forEach(currentTile -> {
+      int tileId = currentTile.getTileId();
+      java.util.stream.IntStream.rangeClosed(1, 12)
+          .filter(j -> tileId + j <= 90)
+          .forEach(j -> currentTile.setNextTile(tiles.get(tileId + j)));
+    });
+  }
+
 }
