9479684a3d295f362940d9dfad7c3ad5ddeb3950
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-02-05 13:20:37 +0100

    feat(App): project main class

--- src/main/java/edu/ntnu/idi/idatt/App.java
+++ src/main/java/edu/ntnu/idi/idatt/App.java
@@ -1,13 +1,15 @@
 package edu.ntnu.idi.idatt;
 
+import edu.ntnu.idi.idatt.controller.GameController;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        GameController gameController = new GameController();
+        gameController.play();
     }
 }
