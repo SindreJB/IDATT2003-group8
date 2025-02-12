746d529c87937e8779b792d92e0aaab368d76876
Stcwal <stwalmann@outlook.com>
2025-02-12 16:36:46 +0100

    feat(JailAction): Added class for JailAction TileAction

---
+++ src/main/java/edu/ntnu/idi/idatt/models/JailAction.java
@@ -0,0 +1,16 @@
+package edu.ntnu.idi.idatt.models;
+
+public class JailAction implements TileAction {
+  private final int endTile;
+
+  public JailAction(int endtTile) {
+    this. endTile = endtTile;
+  } 
+  
+  @Override
+    public int tileActionResult() {
+        return endTile;
+    }
+  
+}
+ 
