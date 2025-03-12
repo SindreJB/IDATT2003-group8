7a173ceb04ea978f7f72d68273af240586b681e7
Stcwal <stwalmann@outlook.com>
2025-03-12 11:39:19 +0100

    docs(models): javadoc model classes

--- src/main/java/edu/ntnu/idi/idatt/models/Dice.java
+++ src/main/java/edu/ntnu/idi/idatt/models/Dice.java
@@ -2,18 +2,53 @@ package edu.ntnu.idi.idatt.models;
 
 import java.util.List;
 
+/**
+ * The {@code Dice} class represents a collection of dice that can be rolled
+ * together.
+ * It provides methods to roll the dice and retrieve the list of dice.
+ * 
+ * <p>
+ * This class is immutable and thread-safe.
+ * </p>
+ * 
+ * <p>
+ * Example usage:
+ * </p>
+ * 
+ * <pre>{@code
+ * Dice dice = new Dice();
+ * int result = dice.rollDice();
+ * List<Die> diceList = dice.getDice();
+ * }</pre>
+ * 
+ * @author
+ */
 public class Dice {
 
   private final List<Die> dice; // List for the option of adding more dice
 
+  /**
+   * Constructs a Dice object with two Die objects.
+   * The Dice object represents a pair of dice.
+   */
   public Dice() {
     this.dice = List.of(new Die(), new Die()); // Two dice
   }
 
+  /**
+   * Rolls all dice in the collection and returns the sum of their values.
+   *
+   * @return the sum of the values of all rolled dice
+   */
   public int rollDice() {
     return dice.stream().mapToInt(Die::roll).sum();
   }
 
+  /**
+   * Returns the list of dice.
+   *
+   * @return a list of {@link Die} objects representing the dice.
+   */
   public List<Die> getDice() {
     return dice;
   }
--- src/main/java/edu/ntnu/idi/idatt/models/LadderAction.java
+++ src/main/java/edu/ntnu/idi/idatt/models/LadderAction.java
@@ -1,15 +1,52 @@
 package edu.ntnu.idi.idatt.models;
 
+/**
+ * The LadderAction class represents an action that moves a player to a specific
+ * tile
+ * when they land on a ladder in a board game.
+ * 
+ * <p>
+ * This class implements the TileAction interface and provides the
+ * implementation
+ * for the tileActionResult method, which returns the tile number where the
+ * player
+ * should be moved to.
+ * </p>
+ * 
+ * <p>
+ * Example usage:
+ * </p>
+ * 
+ * <pre>
+ * {@code
+ * LadderAction ladderAction = new LadderAction(10);
+ * int result = ladderAction.tileActionResult(); // result will be 10
+ * }
+ * </pre>
+ * 
+ * @author Your Name
+ */
 public class LadderAction implements TileAction {
   private final int endTile;
 
+  /**
+   * Represents an action involving a ladder in a game.
+   * This action moves a player to a specified end tile.
+   *
+   * @param endTile the tile number where the ladder ends
+   */
   public LadderAction(int endTile) {
-      this.endTile = endTile;
+    this.endTile = endTile;
   }
 
+  /**
+   * Executes the action associated with the tile and returns the result.
+   *
+   * @return the end tile position after the action is performed.
+   */
   @Override
   public int tileActionResult() {
-      return this.endTile;
+    return this.endTile;
   }
 
 }
