b4a7503d7083dce4741ec7685768316b24dd00c0
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-04-15 11:09:28 +0200

    feat(JsonHandler): use of google Json library

--- src/main/java/edu/ntnu/idi/idatt/persistence/JsonHandler.java
+++ src/main/java/edu/ntnu/idi/idatt/persistence/JsonHandler.java
@@ -1,45 +1,53 @@
 package edu.ntnu.idi.idatt.persistence;
 
-import java.io.File;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.Reader;
+import java.io.Writer;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
- * Utility class for handling JSON file operations.
- * Provides methods to serialize objects to JSON files and deserialize JSON
- * files to objects.
+ * Utility class for reading and writing objects to/from JSON files using GSON
  */
 public class JsonHandler {
-  private static final ObjectMapper objectMapper = new ObjectMapper();
-
-  static {
-    objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
-  }
+  private static final Gson gson = new GsonBuilder()
+          .setPrettyPrinting()
+          .create();
 
   /**
-   * Writes an object to a JSON file.
+   * Writes an object to a JSON file
    *
-   * @param object   The object to serialize
+   * @param object The object to write
    * @param filePath The path where the JSON file will be saved
-   * @param <T>      The type of the object
-   * @throws IOException If an I/O error occurs
+   * @throws IOException If there's an error writing to the file
    */
-  public static <T> void writeToJson(T object, String filePath) throws IOException {
-    objectMapper.writeValue(new File(filePath), object);
+  public static void writeToJson(Object object, String filePath) throws IOException {
+    Path path = Paths.get(filePath);
+    // Create directories if they don't exist
+    Files.createDirectories(path.getParent());
+
+    try (Writer writer = new FileWriter(filePath)) {
+      gson.toJson(object, writer);
+    }
   }
 
   /**
-   * Reads a JSON file and converts it to an object.
+   * Reads an object from a JSON file
    *
-   * @param filePath  The path to the JSON file
-   * @param valueType The class of the object to be created from the JSON file
-   * @param <T>       The type of the object
-   * @return The deserialized object
-   * @throws IOException If an I/O error occurs
+   * @param <T> The type of object to read
+   * @param filePath The path to the JSON file
+   * @param classOfT The class of the object to read
+   * @return The object read from the JSON file
+   * @throws IOException If there's an error reading from the file
    */
-  public static <T> T readFromJson(String filePath, Class<T> valueType) throws IOException {
-    return objectMapper.readValue(new File(filePath), valueType);
+  public static <T> T readFromJson(String filePath, Class<T> classOfT) throws IOException {
+    try (Reader reader = new FileReader(filePath)) {
+      return gson.fromJson(reader, classOfT);
+    }
   }
 }
