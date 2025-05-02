96228b021bc11dbfdde2fd74e1850d1c6ddd11a6
Sindre Boe <sindre.jentoft.boe@hotmail.no>
2025-05-02 10:24:52 +0200

    refactor(mainmenu): Factored out main menu from start method

--- src/main/java/edu/ntnu/idi/idatt/ui/pages/MainMenu.java
+++ src/main/java/edu/ntnu/idi/idatt/ui/pages/MainMenu.java
@@ -1,4 +1,4 @@
-package edu.ntnu.idi.idatt.ui;
+package edu.ntnu.idi.idatt.ui.pages;
 
 import java.io.File;
 import java.io.IOException;
@@ -6,6 +6,7 @@ import java.util.List;
 
 import edu.ntnu.idi.idatt.model.Player;
 import edu.ntnu.idi.idatt.persistence.CsvHandler;
+import edu.ntnu.idi.idatt.ui.LadderGameBoard;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Insets;
@@ -22,17 +23,30 @@ import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
-public class Page extends Application {
+public class MainMenu extends Application {
   private Stage primaryStage;
 
   @Override
   public void start(Stage primaryStage) {
+    Scene mainMenuScene = createMainMenuScene(primaryStage);
+    primaryStage.setTitle("Boardgame Menu");
+    primaryStage.setScene(mainMenuScene);
+    primaryStage.show();
+  }
+
+  /**
+   * Creates and returns the main menu scene
+   *
+   * @param primaryStage the primary stage
+   * @return the created scene
+   */
+  public Scene createMainMenuScene(Stage primaryStage) {
     this.primaryStage = primaryStage;
 
     // Create background
     VBox menuLayout = new VBox(20);
 
-    // Set up the scene and stage
+    // Set up the scene
     Scene scene = new Scene(menuLayout, 600, 400);
     scene.getStylesheets().add(getClass().getResource("/edu/ntnu/idi/idatt/view/styles.css").toExternalForm());
 
@@ -51,7 +65,7 @@ public class Page extends Application {
     VBox div = new VBox();
     div.getChildren().addAll(newGameButton, loadPlayersButton, createPlayersButton, settingsButton, exitButton);
     div.getStyleClass().addAll(
-        "items-center", "w-200", "h-full", "mt-4", "p-4", "space-y-2");
+            "items-center", "w-200", "h-full", "mt-4", "p-4", "space-y-2");
     newGameButton.getStyleClass().addAll("btn", "btn-primary", "w-full");
     loadPlayersButton.getStyleClass().addAll("btn", "btn-primary", "w-full");
     createPlayersButton.getStyleClass().addAll("btn", "btn-primary", "w-full");
@@ -60,13 +74,10 @@ public class Page extends Application {
 
     // Add actions to buttons
     newGameButton.setOnAction(e -> {
-      LadderGameBoard gameBoard = new LadderGameBoard();
-      Scene gameScene = gameBoard.createGameScene("standard", primaryStage);
-      primaryStage.setScene(gameScene);
-      primaryStage.setTitle("Snakes and Ladders - Standard Game");
+      GamesMenu gamesMenu = new GamesMenu();
+      Scene gamesMenuScene = gamesMenu.createGamesMenuScene(primaryStage);
+      primaryStage.setScene(gamesMenuScene);
     });
-    loadGameButton.setOnAction(e -> System.out.println("Load Game clicked"));
-
 
     loadPlayersButton.setOnAction(e -> loadPlayersFromCsv());
     createPlayersButton.setOnAction(e -> edu.ntnu.idi.idatt.ui.components.CreatePlayersForm.display());
@@ -79,29 +90,7 @@ public class Page extends Application {
     menuLayout.getChildren().addAll(titleLabel, div);
     menuLayout.setBackground(new Background(new BackgroundFill(Color.LIGHTBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
 
-    primaryStage.setTitle("Boardgame Menu");
-    primaryStage.setScene(scene);
-    primaryStage.show();
-  }
-
-  /**
-   * Starts a new game
-   */
-  private void startNewGame() {
-    System.out.println("Starting new game...");
-    try {
-      LadderGameBoard board = new LadderGameBoard();
-      Stage gameStage = new Stage();
-      board.start(gameStage);
-      // Hide menu stage when game starts
-      primaryStage.hide();
-
-      // Show menu again when game stage closes
-      gameStage.setOnCloseRequest(e -> primaryStage.show());
-    } catch (Exception e) {
-      showAlert("Error starting game: " + e.getMessage());
-      e.printStackTrace();
-    }
+    return scene;
   }
 
   /**
