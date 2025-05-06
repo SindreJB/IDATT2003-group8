63c85105435dc44427dc97ac0114c36a87ee9c95
Stcwal <stwalmann@outlook.com>
2025-05-06 12:21:35 +0200

    refactor(wormhole): refactor to work like ladder

--- src/main/java/edu/ntnu/idi/idatt/model/WormholeAction.java
+++ src/main/java/edu/ntnu/idi/idatt/model/WormholeAction.java
@@ -1,28 +1,26 @@
 package edu.ntnu.idi.idatt.model;
 
+import java.util.Objects;
+
+/**
+ * Represents a wormhole action that teleports a player to a different tile.
+ */
 public class WormholeAction implements TileAction {
   private int destinationTileNumber;
+  private String description;
+  private static final String TYPE = "Wormhole";
 
-  public WormholeAction(int destinationTileNumber) {
+  /**
+   * Creates a new wormhole action.
+   *
+   * @param startTileId           The ID of the tile where the wormhole starts
+   * @param destinationTileNumber The ID of the tile where the wormhole leads to
+   */
+  public WormholeAction(int startTileId, int destinationTileNumber) {
     this.destinationTileNumber = destinationTileNumber;
-  }
-
-  public int getDestinationTileNumber() {
-    return destinationTileNumber;
-  }
-
-  public void setDestinationTileNumber(int destinationTileNumber) {
-    this.destinationTileNumber = destinationTileNumber;
-  }
-
-  @Override
-  public String toString() {
-    return "Wormhole to tile " + destinationTileNumber;
-  }
-
-  @Override
-  public String getType() {
-    return "Wormhole";
+    this.description = String.format("Wormhole from %d to %d",
+        startTileId,
+        destinationTileNumber);
   }
 
   @Override
@@ -32,6 +30,47 @@ public class WormholeAction implements TileAction {
 
   @Override
   public String getDescription() {
-    return "Teleports player to tile " + destinationTileNumber;
+    return description;
+  }
+
+  @Override
+  public String getType() {
+    return TYPE;
+  }
+
+  // Getters and setters for JSON serialization
+  public int getDestinationTileNumber() {
+    return destinationTileNumber;
+  }
+
+  public void setDestinationTileNumber(int destinationTileNumber) {
+    this.destinationTileNumber = destinationTileNumber;
+    // Update description if the destination changes
+    if (description != null && description.contains("to")) {
+      String[] parts = description.split("to");
+      if (parts.length > 0) {
+        this.description = parts[0] + "to " + destinationTileNumber;
+      }
+    }
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o)
+      return true;
+    if (o == null || getClass() != o.getClass())
+      return false;
+    WormholeAction that = (WormholeAction) o;
+    return destinationTileNumber == that.destinationTileNumber &&
+        Objects.equals(description, that.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(destinationTileNumber, description);
   }
 }
