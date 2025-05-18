908f20e06aa52b8303f6fd50f43c89998627b875
Stcwal <stwalmann@outlook.com>
2025-05-18 18:57:18 +0200

    feat(ladderaction): ladder action

---
+++ src/main/java/edu/ntnu/idi/idatt/model/actions/LadderAction.java
@@ -0,0 +1,28 @@
+package edu.ntnu.idi.idatt.model.actions;
+
+/**
+ * Represents a ladder that moves a player up the board.
+ */
+public class LadderAction extends TileAbstract {
+
+  /**
+   * Creates a new ladder action.
+   *
+   * @param bottomTile The tile at the bottom of the ladder
+   * @param topTile    The tile at the top of the ladder
+   */
+  public LadderAction(int bottomTile, int topTile) {
+    super(bottomTile, topTile);
+    if (topTile <= bottomTile) {
+      throw new IllegalArgumentException("Top tile must be higher than bottom tile for a ladder");
+    }
+  }
+
+  public String getActionType() {
+    return "LADDER";
+  }
+
+  public String getDescription() {
+    return "Climb ladder from " + getSourceTile() + " to " + getDestinationTile();
+  }
+}
